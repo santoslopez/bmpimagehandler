d24229747f890e3cbe530e5d2df94b0b36dcdac4
santoslopez <santoslopez@users.noreply.github.com>
2017-03-09 19:29:07 -0600

    Add files via upload

--- BMPImageHandler.java
+++ BMPImageHandler.java
@@ -5,8 +5,6 @@ public class BMPImageHandler{
   private static String error = "No se encontro ningun parametro";
   private static String basics = "-basics";
   private static String rotate = "-rotate";
-  private static String mensajeEspera = "Espere un momento";
-  private static String imagenInvalida = "La imagen no se encontro";
   private static String parametrosIncorrectos = "Los parametros son mas de lo esperado";
   private static String comandoInvalido = "El comando es invalido";
   public static void main(String[] args){
@@ -15,22 +13,10 @@ public class BMPImageHandler{
       if(parametros==0){
         System.out.println(error);
       }else if((parametros>=1)&&(parametros<=2)){
-          if(args[0].equals("-basics")&&(args[1].equals(args[1]))){
-            File file = new File(args[1]);
-            if(file.exists()){//verificamos si la imagen existe en el proyecto
-              System.out.println(mensajeEspera);
+          if(args[0].equals(basics)&&(args[1].equals(args[1]))){
               BmpHandlerCore.getInstancia().filter(args[1]);
-            }else{
-              System.out.println(imagenInvalida);
-            }
-          }else if(args[0].equals(args[0])&&(args[1].equals(args[1]))){
-            File imagen = new File(args[1]);
-            if(imagen.exists()){
-              System.out.println(mensajeEspera);
+          }else if(args[0].equals(rotate)&&(args[1].equals(args[1]))){
               BmpHandlerRotator.getInstancia().rotateImage(args[1]);
-            }else{
-              System.out.println(imagenInvalida);
-            }
           }else{
             System.out.println(comandoInvalido);
           }
--- BmpHandlerCore.java
+++ BmpHandlerCore.java
@@ -13,6 +13,9 @@ public class BmpHandlerCore{
   private int height;
   private static String nameColor="";
   private static String message="";
+  private static String mensajeEspera = "Espere un momento.";
+  private static String imagenNoEncontrado = "La imagen no se encontro en el proyecto.";
+
   /*
   Hacemos uso de Singleton, sirve para crear una unica instancia de la clase BmpHandlerCore
   */
@@ -40,78 +43,84 @@ public class BmpHandlerCore{
     String generateColor[]={"red","blue","green","sepia","clone"};//arreglo de colores
     String nameColor="";
     if(archivo.exists()){
-      try{
-        for(int pos=0;pos<=4;pos++){
-          nameColor=generateColor[pos];
-          fileInputStream = new FileInputStream(archivo);
-          //obtenemos la longitud de la variable nameImage o del nombre de la imagen que queremos aplicarle el filtro.
-          int lengthNameImage = nameImage.length();
-          //obtenemos unicamente el nombre de la imagen sin la extension ( ".bmp",".jpg",".png",etc )
-          String name = nameImage.substring(0,lengthNameImage-4);
-          //Creamos el archivo, con las caracteristicas que piden en el proyecto ( nombreImagen-nombreColor.bmp)
-          fileOutputStream = new FileOutputStream(name+guion+nameColor+".bmp" );
-          //Leemos los primeros 54 bytes del header
-          for(int position=0;position<54;position++){
-            header[position]=(byte)fileInputStream.read();
-          }
-          /*Hacemos un corrimiento de bits a la izquierda*/
-          width = (((int)header[21]&0xff)<<24)|(((int)header[20]&0xff)<<16)|(((int)header[19]&0xff)<<8)|(((int)header[18]&0xff));
-          height = (((int)header[25]&0xff)<<24)|(((int)header[24]&0xff)<<16)| (((int)header[23]&0xff)<<8)|(((int)header[22]&0xff));
-          if( (getWidth()==640)&((getHeight())==480)){
-            blue=new byte[height][width];
-            green=new byte[height][width];
-            red=new byte[height][width];
-            for(int h=0;h<height;h++){
-              for(int w=0;w<width;w++){
-                red[h][w]=(byte)fileInputStream.read();
-                green[h][w]=(byte)fileInputStream.read();
-                blue[h][w]=(byte)fileInputStream.read();
-              }
+
+      if(nameImage.endsWith(".bmp")){
+        System.out.println(mensajeEspera);
+        try{
+          for(int pos=0;pos<=4;pos++){
+            nameColor=generateColor[pos];
+            fileInputStream = new FileInputStream(archivo);
+            //obtenemos la longitud de la variable nameImage o del nombre de la imagen que queremos aplicarle el filtro.
+            int lengthNameImage = nameImage.length();
+            //obtenemos unicamente el nombre de la imagen sin la extension
+            String name = nameImage.substring(0,lengthNameImage-4);
+            //Creamos el archivo, con las caracteristicas que piden en el proyecto ( nombreImagen-nombreColor.bmp)
+            fileOutputStream = new FileOutputStream(name+guion+nameColor+".bmp" );
+            //Leemos los primeros 54 bytes del header
+            for(int position=0;position<54;position++){
+              header[position]=(byte)fileInputStream.read();
             }
-            fileOutputStream.write(header);//guarda los primeros 54 bytes del header de la imagen
-            for(int h=0;h<getHeight();h++){
-              for(int w=0;w<getWidth();w++){
-                if(nameColor.equals("blue")){
-                  fileOutputStream.write(blue[h][w]);
-                  fileOutputStream.write(0);
-                  fileOutputStream.write(0);
-                }else if(nameColor.equals("green")){
-                  fileOutputStream.write(0);
-                  fileOutputStream.write(green[h][w]);
-                  fileOutputStream.write(0);
-                }else if(nameColor.equals("red")){
-                  fileOutputStream.write(0);
-                  fileOutputStream.write(0);
-                  fileOutputStream.write(red[h][w]);
-                }else if(nameColor.equals("sepia")){
-                  //Esos valores con los que lo multiplique son los recomendados por Microsoft
-                  int newRed = (int)((red[h][w]*0.393) +(green[h][w]*0.769)+(blue[h][w]*0.189));
-                  int newGreen = (int)((red[h][w]*0.349) +(green[h][w]*0.686)+(blue[h][w]*0.168));
-                  int newBlue = (int)((red[h][w]*0.272) +(green[h][w]*0.534)+(blue[h][w]*0.131));
-                  fileOutputStream.write(Math.min(newRed,255));
-                  fileOutputStream.write(Math.min(newGreen,255));
-                  fileOutputStream.write(Math.min(newBlue,255));
-                }else{
-                  fileOutputStream.write(red[h][w]);
-                  fileOutputStream.write(green[h][w]);
-                  fileOutputStream.write(blue[h][w]);
+            /*Hacemos un corrimiento de bits a la izquierda*/
+            width = (((int)header[21]&0xff)<<24)|(((int)header[20]&0xff)<<16)|(((int)header[19]&0xff)<<8)|(((int)header[18]&0xff));
+            height = (((int)header[25]&0xff)<<24)|(((int)header[24]&0xff)<<16)| (((int)header[23]&0xff)<<8)|(((int)header[22]&0xff));
+            if( (getWidth()==640)&((getHeight())==480)){
+              blue=new byte[height][width];
+              green=new byte[height][width];
+              red=new byte[height][width];
+              for(int h=0;h<height;h++){
+                for(int w=0;w<width;w++){
+                  red[h][w]=(byte)fileInputStream.read();
+                  green[h][w]=(byte)fileInputStream.read();
+                  blue[h][w]=(byte)fileInputStream.read();
                 }
               }
+              fileOutputStream.write(header);//guarda los primeros 54 bytes del header de la imagen
+              for(int h=0;h<getHeight();h++){
+                for(int w=0;w<getWidth();w++){
+                  if(nameColor.equals("blue")){
+                    fileOutputStream.write(blue[h][w]);
+                    fileOutputStream.write(0);
+                    fileOutputStream.write(0);
+                  }else if(nameColor.equals("green")){
+                    fileOutputStream.write(0);
+                    fileOutputStream.write(green[h][w]);
+                    fileOutputStream.write(0);
+                  }else if(nameColor.equals("red")){
+                    fileOutputStream.write(0);
+                    fileOutputStream.write(0);
+                    fileOutputStream.write(red[h][w]);
+                  }else if(nameColor.equals("sepia")){
+                    //Esos valores con los que lo multiplique son los recomendados por Microsoft
+                    int newRed = (int)((red[h][w]*0.393) +(green[h][w]*0.769)+(blue[h][w]*0.189));
+                    int newGreen = (int)((red[h][w]*0.349) +(green[h][w]*0.686)+(blue[h][w]*0.168));
+                    int newBlue = (int)((red[h][w]*0.272) +(green[h][w]*0.534)+(blue[h][w]*0.131));
+                    fileOutputStream.write(Math.min(newRed,255));
+                    fileOutputStream.write(Math.min(newGreen,255));
+                    fileOutputStream.write(Math.min(newBlue,255));
+                  }else{
+                    fileOutputStream.write(red[h][w]);
+                    fileOutputStream.write(green[h][w]);
+                    fileOutputStream.write(blue[h][w]);
+                  }
+                }
+              }
+              fileInputStream.close();
+              fileOutputStream.close();
+              System.out.println("La imagen "+nameColor+ " fue generado correctamente");
+            }else{
+              System.out.println("El ancho " + width + " y alto " + height + " de la imagen son dimensiones incorrectas");
+              System.out.println("Dimensiones esperadas: 640x480");
+              break;
             }
-            fileInputStream.close();
-            fileOutputStream.close();
-            System.out.println("La imagen "+nameColor+ " fue generado correctamente");
-          }else{
-            System.out.println("El ancho " + width + " y alto " + height + " de la imagen son dimensiones incorrectas");
-            System.out.println("Dimensiones esperadas: 640x480");
-            break;
           }
+        } catch(Exception exception){
+          exception.printStackTrace();
         }
-      } catch(Exception exception){
-        exception.printStackTrace();
+      }else{
+        System.out.println("El formato de la imagen no es .bmp");
       }
     }else{
-      System.out.println("La imagen no se encontro en el proyecto.");
+      System.out.println(imagenNoEncontrado);
     }
     return nameImage;
   }
